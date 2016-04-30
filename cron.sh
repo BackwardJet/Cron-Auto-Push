9db2bf0823aa77699e6534bbd24bc2a73d3cee10
tejvuligonda <tejvuligonda@gmail.com>
2016-04-30 00:10:35 -0700

    Created log folder and updated script to generate files in the log folder

--- cron.sh
+++ cron.sh
@@ -2,16 +2,16 @@
 
 cd ~/Developer/GitHub/Cron-Auto-Push
 
-touch "$(date).txt"
+touch "log/$(date).txt"
  
  
- git add * >> "$(date).txt"
+ git add * >> "log/$(date).txt"
  #if [[ $? != 0 ]] then 
  #   mail -s "add failed" someone@some.com
  #   exit 1
  #fi
  
- git commit -a -m "$(date)" >> "$(date).txt"
+ git commit -a -m "Auto-commit at $(date)" >> "log $(date).txt"
  #if [[ $? != 0 ]] then 
  #   mail -s "commit failed" someone@some.com
  #   exit 1
