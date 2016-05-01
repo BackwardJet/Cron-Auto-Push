9cd983d44cc5403b8191e7854d19c3994be1575a
tejvuligonda <tejvuligonda@gmail.com>
2016-05-01 14:19:22 -0700

    Got rid of commented out code, updated script to pipe commit info to log

--- auto-push.sh
+++ auto-push.sh
@@ -23,26 +23,7 @@ done
 
 if [ $containsLogFromToday == 0 ]; then
 	git add * >> "$filepath"
-	git commit -a -m "Auto-commit at $date_var"
+	git commit -a -m >> "Auto-commit at $date_var"
 	#git push
 	#git push -u origin master
 fi
- #git add * >> "$filepath"
- #if [[ $? != 0 ]] then 
- #   mail -s "add failed" someone@some.com
- #   exit 1
- #fi
- 
- #git commit -a -m "Auto-commit at $date_var" >> "$filepath"
- #if [[ $? != 0 ]] then 
- #   mail -s "commit failed" someone@some.com
- #   exit 1
- #fi
- 
- #git push
- #if [[ $? != 0 ]] then 
- #   mail -s "push failed" someone@some.com
- #   exit 1
- #fi
- 
- #mail -s "push ok" someone@some.commail -s "push ok" someone@some.com
