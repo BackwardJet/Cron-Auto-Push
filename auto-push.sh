dc6878f8d1f3bfe32d62658edd9bcbb0f467084a
tejvuligonda <tejvuligonda@gmail.com>
2016-05-01 14:29:00 -0700

    Auto-commit at Sun-May--1-14:29:00-PDT-2016

--- auto-push.sh
+++ auto-push.sh
@@ -6,24 +6,22 @@ date_var="$(date)"
 date_var="${date_var// /-}" #replaces all spaces with dashes
 filepath="log/$date_var.log"
 day="${filepath:0:14}" #gets the first 10 characters of string
-echo $day
-echo $filepath
 
 containsLogFromToday=0
 
-for filename in log/*.log; do
-	fileday=${filename:0:14}
-	if [ $fileday == $day ]; then
-		echo "Already auto-committed today"
-		containsLogFromToday=1
-		break
-    fi
-done
+#for filename in log/*.log; do
+#	fileday=${filename:0:14}
+#	if [ $fileday == $day ]; then
+#		echo "Already auto-committed today"
+#		containsLogFromToday=1
+#		break
+#    fi
+#done
  
 
 if [ $containsLogFromToday == 0 ]; then
 	git add * >> "$filepath"
-	git commit -a -m >> "Auto-commit at $date_var"
+	git commit -a -m "Auto-commit at $date_var" >> "$filepath"
 	#git push
 	#git push -u origin master
 fi
