406f7132d0bf6f04ada0bdd3fc4f6ab96eed666e
tejvuligonda <tejvuligonda@gmail.com>
2016-05-01 14:32:27 -0700

    Script working, running crontab

--- auto-push.sh
+++ auto-push.sh
@@ -9,19 +9,19 @@ day="${filepath:0:14}" #gets the first 10 characters of string
 
 containsLogFromToday=0
 
-#for filename in log/*.log; do
-#	fileday=${filename:0:14}
-#	if [ $fileday == $day ]; then
-#		echo "Already auto-committed today"
-#		containsLogFromToday=1
-#		break
-#    fi
-#done
+for filename in log/*.log; do
+	fileday=${filename:0:14}
+	if [ $fileday == $day ]; then
+		echo "Already auto-committed today"
+		containsLogFromToday=1
+		break
+    fi
+done
  
 
 if [ $containsLogFromToday == 0 ]; then
 	git add * >> "$filepath"
-	git commit -a -m "Auto-commit at $date_var" >> "$filepath"
+	git commit -a -m "Auto-commit at $date_var" >> "$filepath""
 	#git push
 	#git push -u origin master
 fi
