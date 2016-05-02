8ad1931eb16c6f34bfa8f07b1f41b59b02a1c5cd
tejvuligonda <tejvuligonda@gmail.com>
2016-05-02 02:32:46 -0700

    Auto-commit at Mon-May--2-02:32:46-PDT-2016

--- auto-push.sh
+++ auto-push.sh
@@ -9,9 +9,26 @@ day="${filepath:0:14}" #gets the first 10 characters of string
 
 containsLogFromToday=0
 
+add_commit_push()
+{
+	echo "-------ADD COMMENTS-------" >> $filepath
+	git add * >> "$filepath"
+	echo "-------COMMIT COMMENTS-------" >> "$filepath"
+    git commit -a -m "Auto-commit at $date_var" >> "$filepath"
+	echo "-------PUSH COMMENTS-------" >> "$filepath"
+    git push -u origin master >> "$filepath"
+	echo "success!"
+}
+
+
+
+if [[ $1 == "-o" ]]; then
+	add_commit_push
+fi
+
 for filename in log/*.log; do
 	fileday=${filename:0:14}
-	if [ $fileday == $day && $1 != "-o" ]; then
+	if [[ $fileday == $day ]] && [[ $1 != "-o" ]]; then # $1 is the first command-line argument, like sys.argv[1] in python
 		echo "Already auto-committed today"
 		containsLogFromToday=1
 		break
@@ -20,8 +37,7 @@ done
  
 
 if [ $containsLogFromToday == 0 ]; then
-	git add * >> "$filepath"
-	git commit -a -m "Auto-commit at $date_var" >> "$filepath"
-	#git push
-	git push -u origin master
+	add_commit_push
 fi
+
+
